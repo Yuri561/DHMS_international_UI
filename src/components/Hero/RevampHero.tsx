c1471f6af747b073c2926bae57fcee1d6f4ba6a3
Houbenove (Yuri) Pierre-Louis <121592752+Yuri561@users.noreply.github.com>
2025-06-20 10:34:09 -0400

    Update RevampHero.tsx

--- src/components/Hero/RevampHero.tsx
+++ src/components/Hero/RevampHero.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import OffsetLink from '../Handler/OffsetLink';
 import heroPic from '../../assets/hero.png';
+import { motion } from 'framer-motion';
 
 const RevampHero: React.FC = () => {
   return (
     <section className="w-full bg-[#d5a86b] mt-12 px-6 py-16 md:py-24 grid grid-cols-1 md:grid-cols-2 items-center max-w-8xl mx-auto font-raleway">
       
       {/* Left Column - Text */}
-      <div className="flex flex-col items-center md:items-center justify-center text-center md:text-right px-4 space-y-6">
-        <div className="text-nowrap items-center justify-center pl-9 flex flex-col">
-          <h1 className="text-8xl sm:text-5xl md:text-6xl lg:text-7xl font-[satisfy] text-white leading-tight">
+      <motion.div 
+        initial={{ opacity: 0, x: -50 }} 
+        whileInView={{ opacity: 1, x: 0 }} 
+        transition={{ duration: 0.8 }}
+        className="flex flex-col items-center md:items-center justify-center text-center md:text-right px-4 space-y-6"
+      >
+        <div className="text-nowrap items-center justify-center pl-4 md:pl-9 flex flex-col">
+          <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-[satisfy] text-white leading-tight">
             Where Culture Meets Beauty
           </h1>
-          <p className="text-md sm:text-lg text-white mt-4">
+          <p className="text-base sm:text-lg text-white mt-4 max-w-xs sm:max-w-sm md:max-w-md">
             Unearth timeless fashion, luminous beauty, and care inspired by heritage.
           </p>
           <OffsetLink 
@@ -22,17 +28,22 @@ const RevampHero: React.FC = () => {
             Shop Now
           </OffsetLink>
         </div>
-      </div>
+      </motion.div>
 
       {/* Right Column - Hero Image */}
-      <div className="flex justify-center items-center mt-12 md:mt-0">
+      <motion.div 
+        initial={{ opacity: 0, x: 50 }} 
+        whileInView={{ opacity: 1, x: 0 }} 
+        transition={{ duration: 0.8 }}
+        className="flex justify-center items-center mt-12 md:mt-0"
+      >
         <img 
           src={heroPic} 
           alt="hero" 
-          className="w-64 sm:w-80 md:w-[26rem] object-contain mask-b-from-20% mask-b-to-80%"
+          className="w-72 sm:w-96 md:w-[32rem] object-contain mask-b-from-20% mask-b-to-80%"
         />
-      </div>
-      
+      </motion.div>
+
     </section>
   );
 };
